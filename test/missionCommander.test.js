d1ecfb5168bd1ccbfcc51aa3078c88087a43ebae
Julieta Delgado <julidmeza@gmail.com>
2022-04-19 23:22:54 -0500

    Make the fail test

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -3,6 +3,6 @@ const MissionCommander = require('../app/missionCommander');
 describe('Unit test for Mission Commander', () => {
     test('1. Create Mission Commander', () => {
         const myMissionCommander = new MissionCommander('Julieta Delgado')
-        expect(myMissionCommander.name).toBe('Julieta Delgado')
+        expect(myMissionCommander.name).toBe('Julieta Delgado Meza')
     })
 })
