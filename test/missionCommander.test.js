3b877c75f830ff511b190aa8374ff389377cc92f
Julieta Delgado <julidmeza@gmail.com>
2022-04-19 23:32:39 -0500

    Two test in same file (failed and passed)

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -1,8 +1,12 @@
 const MissionCommander = require('../app/missionCommander');
 
 describe('Unit test for Mission Commander', () => {
-    test('1. Create Mission Commander', () => {
+    test('1. Create Mission Commander (failed)', () => {
         const myMissionCommander = new MissionCommander('Julieta Delgado')
         expect(myMissionCommander.name).toBe('Julieta Delgado Meza')
+    })
+    test('2. Create Mission Commander (passed)', () => {
+        const myMissionCommander = new MissionCommander('Julieta Delgado')
+        expect(myMissionCommander.name).toBe('Julieta Delgado')
     })
 })
